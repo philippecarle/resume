5fc473cf841ab7335cb57fe161f59d6cb81f463a
Philippe Carle <c@rlephilip.pe>
2022-11-06 09:07:43 -0700

    Improve Skills render

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,22 +1,20 @@
 import Skill from "./Skill";
 import {Col, Row} from "react-bootstrap";
-import Separator from "./Separator";
 import React from 'react';
 
 const Skills = function ({skills}) {
     const half = Math.ceil(skills.length / 2);
 
-    const skillRenderer = (skill, index, array) => (
-        <div key={skill.name}>
+    const skillRenderer = (skill) => (
+        <Col md={6} className={'mb-3'} key={skill.name}>
             <Skill skill={skill}/>
-            <Separator index={index} array={array} span={6} offset={6}/>
-        </div>
+        </Col>
     );
 
     return (
         <Row>
-            <Col md={6}>{skills.slice(0, half).map(skillRenderer)}</Col>
-            <Col md={6}>{skills.slice(half).map(skillRenderer)}</Col>
+            {skills.slice(0, half).map(skillRenderer)}
+            {skills.slice(half).map(skillRenderer)}
         </Row>
     )
 };
