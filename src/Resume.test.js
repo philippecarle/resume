5bf4a2d8d6aa4734b01e5617db990302b3569b83
Philippe Carle <c@rlephilip.pe>
2022-11-05 21:51:13 -0700

    Write first basic tests

--- src/Resume.test.js
+++ src/Resume.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
 import Resume from './Resume';
 
-test('renders Full Resume', () => {
-  render(<Resume />);
-  const linkElement = screen.getByText(/Philippe Carle/i);
-  expect(linkElement).toBeInTheDocument();
+test('The basics data are displayed', () => {
+    render(<Resume/>);
+    expect(screen.getByText('Philippe CARLE')).toBeInTheDocument();
+    expect(screen.getByText('c@rlephilip.pe')).toBeInTheDocument();
 });
+
+
+test('All Section titles are rendered', () => {
+    render(<Resume/>);
+    const headings = screen.getAllByRole('heading', { level: 2 });
+
+    expect(headings[0]).toContainHTML('Experience');
+    expect(headings[1]).toContainHTML('Interests');
+    expect(headings[2]).toContainHTML('Volunteering');
+    expect(headings[3]).toContainHTML('Certificates');
+    expect(headings[4]).toContainHTML('Skills');
+    expect(headings[5]).toContainHTML('Education');
+    expect(headings[6]).toContainHTML('References');
+})
